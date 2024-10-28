86987d097930a5382bac40d546c4ac59e5ee2d99
AmineAffif <affif.amine@live.fr>
2024-10-28 23:03:46 +0100

    (Repairs): Add interventions

---
+++ app/controllers/api/v1/interventions_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::InterventionsController < ApplicationController
+  before_action :authenticate_user, only: [:create, :update, :destroy]
+
+  def index
+    response = FirebaseRestClient.firestore_request('interventions')
+    interventions = FirebaseRestClient.parse_firestore_documents(response)
+    render json: interventions, status: :ok
+  end
+
+  def show
+    response = FirebaseRestClient.firestore_request("interventions/#{params[:id]}")
+    intervention = FirebaseRestClient.parse_firestore_document(response)
+    if intervention
+      render json: intervention, status: :ok
+    else
+      render json: { error: "Intervention not found" }, status: :not_found
+    end
+  end
+
+  def create
+    intervention_data = params.require(:intervention).permit(:repairId, :date, :description)
+    document = FirebaseRestClient.firestore_request('interventions', :post, intervention_data.to_h)
+  
+    if document
+      render json: { id: document['name'].split('/').last, **intervention_data }, status: :created
+    else
+      render json: { error: "Erreur lors de l'ajout de l'intervention" }, status: :internal_server_error
+    end
+  end  
+
+  def update
+    intervention_data = params.require(:intervention).permit(:repairId, :date, :description)
+    document = FirebaseRestClient.firestore_request("interventions/#{params[:id]}", :patch, intervention_data.to_h)
+
+    if document
+      render json: { status: "intervention mise à jour avec succès", document_data: document }, status: :ok
+    else
+      render json: { error: "Erreur lors de la mise à jour de l'intervention" }, status: :internal_server_error
+    end
+  end
+
+  def destroy
+    FirebaseRestClient.firestore_request("interventions/#{params[:id]}", :delete)
+    render json: { status: "intervention supprimée avec succès" }, status: :ok
+  end
+
+  def by_repair
+    repair_id = params[:repair_id]
+    response = FirebaseRestClient.firestore_request('interventions')
+    interventions = FirebaseRestClient.parse_firestore_documents(response)
+
+    repair_interventions = interventions.select { |intervention| intervention[:repairId] == repair_id }
+    render json: repair_interventions, status: :ok
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
       resources :clients, only: [:index, :show, :create, :update, :destroy]
       resources :vehicles, only: [:index, :show, :create, :update, :destroy]
       resources :repairs, only: [:index, :show, :create, :update, :destroy]
-      resources :interventions, only: [:index, :show, :create, :update, :destroy]
+      resources :interventions, only: [:index, :show, :create, :update, :destroy] do
+        get 'by_repair/:repair_id', to: 'interventions#by_repair', on: :collection
+      end
       resources :notifications, only: [:index, :show, :create, :update, :destroy]
       resources :customers, only: [:index, :show, :create, :update, :destroy] do
         get 'vehicles', to: 'vehicles#by_customer'
