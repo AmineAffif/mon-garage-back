fdb4837e57132b75f89e093cbc72d7f654670ba3
AmineAffif <affif.amine@live.fr>
2024-11-01 15:55:07 +0100

    fix repair retreive vehicle route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       resources :vehicles, only: [:index, :show, :create, :update, :destroy] do
         collection do
           get 'by_customer', to: 'vehicles#by_customer'
+          get 'by_customer/:firebaseAuthUserId', to: 'vehicles#by_customer'
         end
       end
 
