fdb6d05bf52a7f347939d43075477a1208ea7fed
AmineAffif <affif.amine@live.fr>
2024-11-01 19:23:51 +0100

    cors api auth

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'https://mon-garage-d73hunanv-amine-affifs-projects.vercel.app' : 'http://localhost:3001'
+    origins 'http://localhost:3001', 'https://mon-garage-d73hunanv-amine-affifs-projects.vercel.app'
 
     resource '/api/*',
              headers: :any,
